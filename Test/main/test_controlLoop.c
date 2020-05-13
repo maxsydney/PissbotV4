7fb150a13f6cb2f2cc6aff1b8eee5652c55a1a01
Max Sydney <maxsydney8@gmail.com>
2020-05-13 23:44:51 +1200

    Removing old test junk

--- Test/main/test_controlLoop.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "../../../main/controlLoop.h"
-#include "unity.h"
-
-TEST_CASE("VapourConcentration", "[controlLoop]")
-{
-    // TEST_ASSERT_EQUAL(computeVapourEthConcentration(78.1), 100);  
-    TEST_ASSERT_EQUAL(100, 100);
-}
--- Test/willItCompile.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "pump.h"
- 
-int main(int argc, char *argv[]){
-    Pump p;
-    p.setSpeed(500);
-    std::cout << "Pump speed " << p.getSpeed() << std::endl;
-    return 0;
-}
