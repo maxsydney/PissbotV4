624db94378ac2dafbf54edc6492717ac745174e9
Max Sydney <maxsydney8@gmail.com>
2020-09-01 22:50:06 +1200

    Initial commit for distiller manager

---
+++ main/distillerManager.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_DISTILLERMANAGER_H
+#define MAIN_DISTILLERMANAGER_H
+
+#include "controller.h"
+
+// Main system manager class. Monitors all sub components
+class DistillerManager
+{
+    public:
+
+    private:
+        Controller _controller {};
+
+};
+
+#endif // MAIN_DISTILLERMANAGER_H
