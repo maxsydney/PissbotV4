10bb7ff5c1ddf3c89570290f40c9e8ad3a471e39
Max Sydney <maxsydney8@gmail.com>
2020-09-28 22:28:32 +1300

    more scaffolding

--- main/distillerManager.h
+++ main/distillerManager.h
@@ -1,14 +1,20 @@
 #ifndef MAIN_DISTILLERMANAGER_H
 #define MAIN_DISTILLERMANAGER_H
 
+#include "tasklet.hpp"
 #include "controller.h"
 
 // Main system manager class. Monitors all sub components
 class DistillerManager
 {
-    public:
+    static constexpr char* name = "Distiller Manager";
 
+    public:
+        DistillerManager(void) = default;
+       
     private:
+
+        int _testInt = 0;
         Controller _controller {};
 
 };
