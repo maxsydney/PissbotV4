a8efbbc456b7c59f22d86059e47cc09dc4d302a6
Max Sydney <maxsydney8@gmail.com>
2020-11-24 22:30:50 +1300

    Save makeFlashImg

--- unitTest/createFlashImg.sh
+++ unitTest/createFlashImg.sh
@@ -6,7 +6,7 @@ arg_flashimg=$2
 if [ -z "$1" -o -z "$2" ]; then
     echo "Usage: createFlashImg.sh app_name flash_img_file"
     exit 1
-fi
+fi 
 
 dd if=/dev/zero bs=1024 count=4096 of=${arg_flashimg}
 dd if=build/bootloader/bootloader.bin bs=1 seek=$((0x1000)) of=${arg_flashimg} conv=notrunc
